467ba879afcc4cd1c3cdd4beb7a5b416a82f5606
Shyam-45 <shyam14027@gmail.com>
2025-08-20 20:06:27 +0530

    feat: authentication added

---
+++ lib/screens/chat.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chats"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              try {
+                await FirebaseAuth.instance.signOut();
+              } on FirebaseAuthException catch (error) {
+                if (!context.mounted) return;
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text(error.message ?? 'Logout failed')),
+                );
+              }
+            },
+            icon: Icon(Icons.exit_to_app),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ],
+      ),
+      body: const Center(child: Text("chat screen")),
+    );
+  }
+}
